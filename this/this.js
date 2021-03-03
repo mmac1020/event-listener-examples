7bee26e1ba7ab7d71d217f1198754af27d48c76e
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-03-03 17:35:24 -0500

    Adding examples from 2103-ghp

--- this/this.js
+++ this/this.js
@@ -1,13 +1,30 @@
-const buttonContainer = document.getElementById('buttonContainer');
+const mac = {
+  name: 'mac',
+  sayHi() {
+    console.log(`Hello, my name is ${this.name}`);
+  },
+  sayHiArrow: () => {
+    console.log(`Hello, my name is ${this.name}`);
+  },
+};
+const Elisa = { name: 'Elisa' };
+mac.sayHi.call(mac);
+const elisaSayHi = mac.sayHi.bind(Elisa);
+elisaSayHi();
+mac.sayHi();
+// mac.sayHi();
+// mac.sayHiArrow();
 
-const buttonOne = document.getElementById('buttonOne');
+// const buttonContainer = document.getElementById('buttonContainer');
 
-function mouseOver (event) {
-  console.log(`event: ${event}`);
-  console.log(`target: ${event.target}`);
-  console.log(`this: ${this}`);
-}
+// const buttonOne = document.getElementById('buttonOne');
 
-buttonContainer.addEventListener('mouseover', mouseOver);
+// function mouseOver (event) {
+//   console.log(`event: ${event}`);
+//   console.log(`target: ${event.target}`);
+//   console.log(`this: ${this}`);
+// }
 
-buttonOne.addEventListener('mouseover', mouseOver);
+// buttonContainer.addEventListener('mouseover', mouseOver);
+
+// buttonOne.addEventListener('mouseover', mouseOver);
